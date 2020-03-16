34fcb542e072ee1b5fc2689013d0bdd910dbf25a
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2020-03-16 19:25:28 +0100

    Add filter.svg icon

--- src/components/FilterSelectVoice.vue
+++ src/components/FilterSelectVoice.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="relative inline-block w-64">
-    <select
-      v-model="selectInput"
-      @input="onSelectInput($event)"
-      class="block w-full px-4 py-2 pr-8 bg-black rounded shadow appearance-none select focus:outline-none"
-    >
-      <option>All</option>
-      <option :key="option" v-for="option in options">{{ option }}</option>
-    </select>
-    <div
-      class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none"
-    >
-      <img src="@/assets/select-arrow.svg" />
+  <div class="flex">
+    <img src="@/assets/filter.svg" />
+    <div class="relative inline-block w-64 ml-2">
+      <select
+        v-model="selectInput"
+        @input="onSelectInput($event)"
+        class="block w-full px-4 py-2 pr-8 bg-black rounded shadow appearance-none select focus:outline-none"
+      >
+        <option>All</option>
+        <option :key="option" v-for="option in options">{{ option }}</option>
+      </select>
+      <div
+        class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none"
+      >
+        <img src="@/assets/select-arrow.svg" />
+      </div>
     </div>
   </div>
 </template>
@@ -43,4 +46,8 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+select {
+  -webkit-appearance: none;
+}
+</style>
