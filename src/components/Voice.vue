1f38603d3a25210f83b12fd49a9ade009d2302ae
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2020-03-16 13:59:08 +0100

    Add favorite voice buton

--- src/components/Voice.vue
+++ src/components/Voice.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="text-center cursor-pointer voice">
+  <div class="relative text-center cursor-pointer voice">
+    <p
+      class="absolute invisible w-6 h-6 bg-gray-100 rounded-full voice-favourite-container"
+    >
+      <img
+        class="voice-favourite"
+        src="@/assets/voice-favourite-off.svg"
+        alt="favourite voice"
+      />
+    </p>
     <img
-      class="inline bg-gray-500 rounded-full"
+      class="inline bg-gray-500 rounded-full voiceImg"
       :src="require(`@/assets/${icon}`)"
       :alt="name"
     />
@@ -27,11 +36,24 @@ export default {
 </script>
 
 <style scoped>
-.voice:hover img {
+.voice:hover img.voiceImg {
   @apply bg-gray-100;
 }
 
 .voice:hover p {
   @apply text-gray-100;
+}
+
+.voice:hover .voice-favourite-container {
+  @apply visible;
+}
+
+.voice-favourite-container {
+  right: 0.25rem;
+}
+
+.voice-favourite {
+  padding-left: 5px;
+  padding-top: 6px;
 }
 </style>
